31ef6bbdb66cfeee00987a4a01b4fda8aa38ee20
omar <you@example.com>
2026-03-29 23:13:22 +0200

    profile signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from  django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -8,8 +10,12 @@ class Profile(models.Model):
     image = models.ImageField( upload_to='profile/', )
     phone = models.CharField(max_length= 15)
 
-
-
+# Signal  create new use --> create new profile
+@receiver(post_save, sender=User) 
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    
 
 
 class City(models.Model):
