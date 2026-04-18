be00f220231e8b45ea57bab1a08c4eba8edd7800
omar <omarmohamed99952@gmail.com>
2026-04-18 14:23:53 +0200

    modify urls.py before deploy

--- project/urls.py
+++ project/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 urlpatterns = [
+    # Redirect the root to /jobs/
+    path('', RedirectView.as_view(url='/jobs/', permanent=True)),
+    
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls',namespace='accounts')),
     path('admin/', admin.site.urls),
